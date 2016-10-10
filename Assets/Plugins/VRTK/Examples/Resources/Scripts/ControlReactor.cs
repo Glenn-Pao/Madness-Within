ff450654e53ae5a7218eca9b6b13d1e4b45d56af
Glenn-Pao <thelolfactor@gmail.com>
2016-10-10 18:04:06 +0800

    Updated VRTK component.

--- Assets/Plugins/VRTK/Examples/Resources/Scripts/ControlReactor.cs
+++ Assets/Plugins/VRTK/Examples/Resources/Scripts/ControlReactor.cs
@@ -4,48 +4,17 @@
 
     public class ControlReactor : MonoBehaviour
     {
-        // Masujima
-        public enum DisplayMode
-        {
-            HandleChange,
-            HandleChangeWithTextName
-        }
-
         public TextMesh go;
-        private string temp;
-
-        // Masujima
-        public DisplayMode mode;
 
         private void Start()
         {
-            temp = go.text;
-
-
-            // Masujima
-            switch (mode)
-            {
-                case DisplayMode.HandleChange:
-                    GetComponent<VRTK_Control>().defaultEvents.OnValueChanged.AddListener(HandleChange);
-                    HandleChange(GetComponent<VRTK_Control>().GetValue(), GetComponent<VRTK_Control>().GetNormalizedValue());
-                    break;
-                case DisplayMode.HandleChangeWithTextName:
-                    GetComponent<VRTK_Control>().defaultEvents.OnValueChanged.AddListener(HandleChangeWithTextName);
-                    HandleChangeWithTextName(GetComponent<VRTK_Control>().GetValue(), GetComponent<VRTK_Control>().GetNormalizedValue());
-                    break;
-                default:
-                    break;
-            }
+            GetComponent<VRTK_Control>().defaultEvents.OnValueChanged.AddListener(HandleChange);
+            HandleChange(GetComponent<VRTK_Control>().GetValue(), GetComponent<VRTK_Control>().GetNormalizedValue());
         }
 
         private void HandleChange(float value, float normalizedValue)
         {
-           go.text = value.ToString() + "(" + normalizedValue.ToString() + "%)";
-        }
-        // Masujima
-        private void HandleChangeWithTextName(float value, float normalizedValue)
-        {
-            go.text = temp + " " + value.ToString() + "(" + normalizedValue.ToString() + "%)";
+            go.text = value.ToString() + "(" + normalizedValue.ToString() + "%)";
         }
     }
 }
