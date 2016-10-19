e8629b8864e4e2c653eecc65bdebbfb3ff5ac7fc
Glenn-Pao <140522J@mymail.nyp.edu.sg>
2016-10-19 11:56:00 +0800

    Haven't finished..

---
+++ Assets/Scripts/Gameplay/Door/Hammer.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+//this is the hammer component of the chisel & hammer mechanics
+public class Hammer : MonoBehaviour
+{
+    public GameObject HammerHead;
+
+	// Use this for initialization
+	void Start ()
+    {
+	    
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
