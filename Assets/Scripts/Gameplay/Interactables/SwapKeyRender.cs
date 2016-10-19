2f7ff9f959e494eef3b67112e5cc52830dcd4e08
Glenn-Pao <140522J@mymail.nyp.edu.sg>
2016-10-19 10:53:49 +0800

    Added some particle effects that might come useful later.

---
+++ Assets/Scripts/Gameplay/Interactables/SwapKeyRender.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+//This script handles the rendering of the key on the key hole knob
+public class SwapKeyRender : MonoBehaviour 
+{
+	public GameObject otherKey;		//the other key
+
+	void OnCollisionEnter(Collision other)
+	{
+		if (other.gameObject.tag == "Door") 
+		{
+			otherKey.SetActive (true);
+			Destroy (this.gameObject);
+		}
+	}
+	// Use this for initialization
+	void Start () 
+	{
+		if (otherKey.gameObject.activeInHierarchy) 
+		{
+			otherKey.gameObject.SetActive (false);
+		}
+	}
+	
+}
