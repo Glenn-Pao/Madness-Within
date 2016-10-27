0cb8acfc55e060c97ddcf32bbf10f1cf124167a8
Glenn-Pao <thelolfactor@gmail.com>
2016-10-27 17:06:54 +0800

    Added GUI texture. Going to do fade in and fade out of texture later

---
+++ Assets/Scripts/Gameplay/ResetPosition.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+//If the player falls off the world, this script attempts to reset the position of the player for them to continue playing
+//This class may be used to create checkpoints next time
+public class ResetPosition : MonoBehaviour 
+{
+	public GameObject CameraVR;		//the camera
+	public Transform initPos;		//initial position of the camera
+
+	//When the player collides to this, reset the position
+	void OnCollisionEnter()
+	{
+		CameraVR.transform.position = initPos.position;
+	}
+	// Use this for initialization
+	void Start () 
+	{
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
